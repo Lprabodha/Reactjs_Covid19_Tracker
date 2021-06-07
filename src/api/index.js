771f65d291f5f91915e709de96c9c426407f2523
lahiru prabodha <67253461+Lprabodha@users.noreply.github.com>
2021-06-07 18:33:55 +0530

    chartjs
    chartjs and cardjs

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,32 @@
 import axios from 'axios';
 
-
+// set api
 const url  = 'https://covid19.mathdro.id/api';
 
+// create fetchData functions
 export const fetchData = async () =>{
     try {
         const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
         return {confirmed,recovered,deaths,lastUpdate};
        
     } catch(error){
+
+    }
+}
+
+// create fetchdailyData functions
+export const fetchDailyData =  async () => {
+    try{
+        const {data} = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData)=>({
+            confirmed:dailyData.confirmed.total,
+            deaths:dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+
+    }catch(error){
 
     }
 }
